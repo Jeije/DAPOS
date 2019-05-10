640a68349d44e5d16daad63b9f786337ccb9ac1f
Martijn <ms.jorissen@tiscali.nl>
2019-05-10 09:22:59 +0200

    with power output

--- Iterated_drag_surface.py
+++ Iterated_drag_surface.py
@@ -7,12 +7,14 @@ Created on Thu May  9 16:48:59 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-massf_thrust_lit = 0.0306834  #[mg/s/mN]
-massf_thrust = massf_thrust_lit*10**-3  #[kg/s/N]
+Isp = 3300   #[s]
+#massf_thrust_lit = 0.0306834  #[mg/s/mN]
+#massf_thrust = massf_thrust_lit*10**-3  #[kg/s/N]
+massf_thrust = 1/(Isp*9.81)
 density = 1*10**-10   #[kg/m^3]
 velocity = 7800     #[m/s]
-intake_eff = 0.4
+intake_eff = 0.4    #[-]
+thrust_power = 20*10**3 #[W/N]
 
 def A_to_Drag(A):
     return density*(velocity**2)*(1.+np.pi/6.)*np.sqrt(A/np.pi)*A
@@ -21,10 +23,10 @@ def Drag_to_A(D):
     return np.cbrt((D/(density*velocity**2*(1+np.pi/6)))**2*np.pi)
 
 def thrust_to_A(T):
-    return T/(density*velocity/massf_thrust)
+    return T/(density*velocity*intake_eff/massf_thrust)
 
 def A_to_thrust(A):
-    return density*A*velocity/massf_thrust
+    return density*A*velocity*intake_eff/massf_thrust
 
 
 A_init = 0.0001   #[m^2]
@@ -54,17 +56,22 @@ while np.abs(thrust_t-drag_t)>0.00000001:
 
 plt.plot(areal, dragl, "red")
 plt.plot(areal,thrustl, "blue")
-plt.show()     
+plt.show()  
+   
+print ("Area = ", area, "[m^2]")
+print ("Thrust/Drag = ", drag, "[N]")
+print ("Massflow =", area*density*velocity*intake_eff, "[kg/s]")
+print ("Power required = ", thrust*thrust_power, "[W]")
 
 
-#area_list = np.arange(0.,100.,0.2)
-#drag_list = []
-#thrust_list = []
-#for i in area_list:
-#    drag_list.append(A_to_Drag(i))
-#    thrust_list.append(A_to_thrust(i))
+area_list = np.arange(0.,10.,0.2)
+drag_list = []
+thrust_list = []
+for i in area_list:
+    drag_list.append(A_to_Drag(i))
+    thrust_list.append(A_to_thrust(i))
     
-#plt.plot(area_list, drag_list, "red")
-#plt.plot(area_list, thrust_list, "blue")
-#plt.show()
+plt.plot(area_list, drag_list, "red")
+plt.plot(area_list, thrust_list, "blue")
+plt.show()
 
